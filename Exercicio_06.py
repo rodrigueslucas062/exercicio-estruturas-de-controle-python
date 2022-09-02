f62c241d50cc33de02602e5887a26601469bbe31
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-09-02 11:00:39 -0300

    Create Exercicio_06.py

---
+++ Exercicio_06.py
@@ -0,0 +1,9 @@
+#Faça um programa que receba o código correspondente ao cargo de um funcionário e seu salário atual e mostre o cargo, o valor do aumento e seu novo salário. Os cargos estão na tabela a seguir.
+
+print('Selecione uma opção:\n1) Escrituário\n2) Secretário/n')
+
+entrada = int(input('Opção: '))
+
+if entrada == 1:
+    numero_1 = float(input('Número 1: '))
+    numero_2 = float(input('Número 2: '))
