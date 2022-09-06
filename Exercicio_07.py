569830ba59242d3317a8b711480ee9486cc787bb
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-09-06 09:07:05 -0300

    Update Exercicio_07.py

--- Exercicio_07.py
+++ Exercicio_07.py
@@ -3,6 +3,13 @@
 
 salario = int(input('Salário: '))
 
-if salario < 499:
-    aumento = int (salario + (escriturario * salario))
-    print(f'O salario atual é de: {salario}, e o aumento é de: {aumento}. O novo salario é de: {salario + aumento}')
+if salario < 500:
+    bonificacao = int (salario + (salario * 0.05))
+    print(f'O salario atual é de: {salario}, e o bonificacao é de: {bonificacao}. O novo salario é de: {bonificacao + 150} com auxilio escola')
+
+if (salario in range(600, 1200)):
+    bonificacao = int (salario + (salario * 0.12))
+    print(f'O salario atual é de: {salario}, e o bonificacao é de: {bonificacao}. O novo salario é de: {bonificacao + 100} com auxilio escola')
+    
+if salario > 1200:
+    print(f'O salario atual é de: {salario + 100}, e não possui bonificação')
