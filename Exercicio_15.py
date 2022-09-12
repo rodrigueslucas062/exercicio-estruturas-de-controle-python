e2fb6739b22be3c2f5dd6d21b485150f4969df6f
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-09-12 00:27:23 -0300

    Create Exercicio_15.py

---
+++ Exercicio_15.py
@@ -0,0 +1,6 @@
+#Foi feita uma pesquisa para determinar o índice de mortalidade infantil em certo período. Faça um programa que:
+
+nascimento = int(input('Nascidos: '))
+sexo = int(input('Sexo: '))
+vida = int(input('Vida: '))
+
